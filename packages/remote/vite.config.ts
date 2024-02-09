fbce04e3701a530cbed0841261a7be4482985591
Jiheon Park <harrypark.dev@gmail.com>
2024-02-10 01:00:17 +0900

    feat: set module federtion config

--- packages/remote/vite.config.ts
+++ packages/remote/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  esbuild: {
+    target: "es2020",
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      // Modules to expose
+      exposes: {
+        "./ReactLogo": "./src/components/ReactLogo",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
